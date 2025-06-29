ef0e290c211804f7f16d3b86b8e62952a96a4584
Vadim <gubaydullin03@internet.ru>
2025-06-29 13:17:47 +0300

    Add models for app user

---
+++ backend/apps/user/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+class User(AbstractUser):
+    """Custom user model for music player"""
+    email = models.EmailField(_('email address'), unique=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    favorite_genre = models.CharField(max_length=100, blank=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
